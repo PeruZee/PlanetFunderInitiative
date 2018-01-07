577b412e131c459c3c35fac1b0a3ec98bfe9ca49
Pruz <per_zaman@live.ca>
2018-01-07 07:04:30 -0500

    Change Trust
    This program prompts user for their SEED, AMOUNT of Asset(TBC) to trust and shows the transaction details as well as their previous account balance.

---
+++ src/TrustChange.java
@@ -0,0 +1,99 @@
+/*
+ * Original: Stellar
+ * Modified: Pruz (07.01.2018)
+ * Reference Libs: stellar-sdk.jar
+ * This program Allows Asset to be Trusted
+ * Prompts user for their account seed and
+ * also prompts user for amount of asset (TBC) to trust and
+ * also shows them their previous balances.
+ * Handles ioexception.
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import org.stellar.sdk.*;
+import org.stellar.sdk.responses.AccountResponse;
+import org.stellar.sdk.responses.SubmitTransactionResponse;
+
+public class TrustChange {
+
+    private static Scanner scanner = new Scanner( System.in );
+
+	public static void main(String[] args) throws IOException {
+
+		Network.useTestNetwork();
+		Server server = new Server("https://horizon-testnet.stellar.org");
+		
+		// Issuing Account Address for Asset: TBC
+		KeyPair  issuingKeys = KeyPair
+				  .fromAccountId("GDZHWATCLKQTIIHEKFJNTJCR234NE6UIZSJKD2VDEPVTJ3ZYZF7CQMZY");
+
+	    // Asks user for Source account seed
+    	System.out.println("\nEnter the source account seed: ");
+    	String input = scanner.nextLine();
+        KeyPair source;
+        
+        try {
+            	source = KeyPair.fromSecretSeed(input);
+            	TimeUnit.SECONDS.sleep(2); //wait 2 seconds
+        }
+        catch (Exception e) {
+            	throw new RuntimeException("Error! Something went wrong!");
+        }
+        
+        // Asks user for amount of Asset (TBC) to Trust
+        try {
+        	System.out.println("\nEnter the amount of Asset(TBC) to Trust: ");
+        	TimeUnit.SECONDS.sleep(2); //wait 2 seconds
+    	}
+        catch (Exception e) {
+        	throw new RuntimeException("Error! Something went wrong!");
+    	}
+        	String amount = scanner.nextLine();
+		
+		// Represent the Asset
+		Asset TBC = Asset.createNonNativeAsset("TBC", issuingKeys);
+
+
+		// Make the receiving account trust the asset
+		AccountResponse receiving = null;
+		try {
+			receiving = server.accounts().account(source);
+		}
+	    catch (Exception e) {
+	        	throw new RuntimeException("Error! Something went wrong!");
+	       	}
+		
+		Transaction allowTBC = new Transaction.Builder(receiving)
+		  .addOperation(
+		// ChangeTrust operation creates (or alters) a Trustline
+		// Second parameter limits the amount
+		    new ChangeTrustOperation.Builder(TBC, amount).build())
+		  .build();
+		allowTBC.sign(source);
+		
+		// Display Ledger number and Transaction Hash if it was a success
+		try {
+			SubmitTransactionResponse res = server.submitTransaction(allowTBC);
+			if (!res.isSuccess() == false) {
+			System.out.println("\nCongrats! It was a success!");
+        	System.out.println("\nLedger Number:\n" + res.getLedger());
+        	System.out.println("Transaction Hash:\n" + res.getHash());
+        	TimeUnit.SECONDS.sleep(1); //wait 1 second
+			}
+		} 
+	    catch (Exception e) {
+        	throw new RuntimeException("\nError! Something went wrong!");
+		}
+		// Get account balances for source account
+        System.out.println("\nBalances for account: " + source.getAccountId());
+	        for (AccountResponse.Balance balance : receiving.getBalances()) {
+	        		System.out.println("\nType: " + balance.getAssetType());
+	        		System.out.println("Code: " + balance.getAssetCode());
+	        		System.out.println("Limit: " + balance.getLimit());
+	        		System.out.println("Balance: " + balance.getBalance());
+      	}
+	}
+	
+}
