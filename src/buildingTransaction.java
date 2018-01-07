8d582e3e969079f9d7c86a5f04408a58981dfd89
Pruz <per_zaman@live.ca>
2018-01-06 23:08:51 -0500

    Hash and Balances
    Now also displays Ledger Number, Transaction Hash and Balances at the end.

--- src/buildingTransaction.java
+++ src/buildingTransaction.java
@@ -5,6 +5,7 @@
  * Now this program sends payments to another account by prompting--
  * --USER for "source account seed", "destination account address" and--
  * --"amount of XLM" from "source" account to "destination" account.
+ * Also displays ledger number, Transaction Hash & Balances at the end.
  * Handles ioexception
  */
 import java.io.*;
@@ -17,7 +18,7 @@ import org.stellar.sdk.responses.SubmitTransactionResponse;
 public class buildingTransaction {
 
     private static Scanner scanner = new Scanner( System.in );
-
+    
 	public static void main(String[] args) throws IOException {
 
 	Network.useTestNetwork();
@@ -34,7 +35,7 @@ public class buildingTransaction {
             source = KeyPair.fromSecretSeed(input);
             TimeUnit.SECONDS.sleep(2); //wait 2 seconds
         } catch (Exception e) {
-            throw new RuntimeException("Error!");
+            throw new RuntimeException("Error! Something went wrong!");
         }
         
     //Asks user for destination account address
@@ -47,12 +48,18 @@ public class buildingTransaction {
             destination = KeyPair.fromAccountId(input2);
             TimeUnit.SECONDS.sleep(2); //wait 2 seconds
         } catch (Exception e) {
-            throw new RuntimeException("Error!");
+            throw new RuntimeException("Error! Something went wrong!");
         }
         
     //Asks user for amount of XLM to be sent
+        try {
         System.out.println("\nEnter the amount of XLM to send: ");
+        TimeUnit.SECONDS.sleep(2); //wait 2 seconds
+    	} catch (Exception e) {
+        throw new RuntimeException("Error! Something went wrong!");
+    	}
         String amount = scanner.nextLine();
+
 
 	//1. Confirm the account ID exists
 	        server.accounts().account(destination);
@@ -73,15 +80,25 @@ public class buildingTransaction {
 	//6. Sign the transaction using "source" Seed
 	        transaction.sign(source);
 
-	//7. Send to Stellar network to submit transaction
+	//7. Send to Stellar network to submit transaction and get Ledger number & Transaction Hash, extras if errors
 	        try {
 	        	SubmitTransactionResponse response = server.submitTransaction(transaction);
-	            System.out.println("Success!");
-	            System.out.println("\n" + response);
+	            System.out.println("\nSuccess! " + amount + " Lumens(XLM) were sent!\n");
+	            System.out.println("Ledger Number:\n" + response.getLedger());
+	            System.out.println("Transaction Hash:\n" + response.getHash());
+	            System.out.println("\nExtras: " + response.getExtras());
 	        }
 	        catch (Exception e) {
-	        	System.out.println("Something went wrong!");
+	        	System.out.println("\nSomething went wrong!");
 	        	System.out.println(e.getMessage());
 	        }
-	    }
+	//8. Get account balances for source account        
+	        AccountResponse sourceAccount1 = server.accounts().account(KeyPair.fromAccountId(source.getAccountId()));
+	        System.out.println("\nBalances for account: \n" + source.getAccountId());
+	        for (AccountResponse.Balance balance : sourceAccount1.getBalances()) {
+	        	System.out.println("\nType: " + balance.getAssetType());
+	        	System.out.println("Code: " + balance.getAssetCode());
+	        	System.out.println("Balance: " + balance.getBalance());
+	        	}
 	}
+}
