310c728e5c9ba58748023acd39bbb422246e3148
Pruz <per_zaman@live.ca>
2018-01-09 02:03:27 -0500

    Account  Code

--- src/AutoAccountTrustLimit.java
+++ src/AutoAccountTrustLimit.java
@@ -19,60 +19,58 @@ import org.stellar.sdk.responses.SubmitTransactionResponse;
 
 //AutoAccountTrustLimit: Class to make the account
 public class AutoAccountTrustLimit {
-	
+
 	private static Scanner scanner = new Scanner( System.in );
-	
+
 	public static void main(String[] args) throws IOException {
-    	
-        //server: Stellar server (TESTNet)
+
+		//server: Stellar server (TESTNet)
 		Network.useTestNetwork();
-        Server server = new Server("https://horizon-testnet.stellar.org");
-        
-    	//Uses random seed to generate account
-        KeyPair pair = KeyPair.random();
-        System.out.println("Secret Seed: " + new String(pair.getSecretSeed()));
-        System.out.println("Account Add: " + pair.getAccountId());
-        
+		Server server = new Server("https://horizon-testnet.stellar.org");
+
+		//Uses random seed to generate account
+		KeyPair pair = KeyPair.random();
+		System.out.println("Secret Seed: " + new String(pair.getSecretSeed()));
+		System.out.println("Account Add: " + pair.getAccountId());
+
 		// Issuing Account Address for Asset: TBC
-		KeyPair  issuingKeys = KeyPair
-				  .fromAccountId("GDZHWATCLKQTIIHEKFJNTJCR234NE6UIZSJKD2VDEPVTJ3ZYZF7CQMZY");
+		KeyPair  issuingKeys = KeyPair.fromAccountId("GDZHWATCLKQTIIHEKFJNTJCR234NE6UIZSJKD2VDEPVTJ3ZYZF7CQMZY");
 
-        //friendbotUrl: Uses TESTNet FriendBot to fund account
-        String friendbotUrl = String.format("https://horizon-testnet.stellar.org/friendbot?addr=%s", pair.getAccountId());
-        InputStream response = new URL(friendbotUrl).openStream();
-        
-        try {
-        	@SuppressWarnings("resource")
-        	String body = new Scanner(response, "UTF-8").useDelimiter("\\A").next();
-        	System.out.println("\nSUCCESS! You have a new TESTNet account!\n" + body);
-        	TimeUnit.SECONDS.sleep(1); //wait 1 second
-        }
-       	catch (Exception e) {
-        	throw new RuntimeException("Error! Something went wrong!");
-       	}
+		//friendbotUrl: Uses TESTNet FriendBot to fund account and display        
+		try {
+			String friendbotUrl = String.format("https://horizon-testnet.stellar.org/friendbot?addr=%s", pair.getAccountId());
+			InputStream response = new URL(friendbotUrl).openStream();
+			@SuppressWarnings("resource")
+			String body = new Scanner(response, "UTF-8").useDelimiter("\\A").next();
+			System.out.println("\nSUCCESS! You have a new TESTNet account!\n" + body);
+			TimeUnit.SECONDS.sleep(1); //wait 1 second
+			}
+		catch (Exception e) {
+			throw new RuntimeException("Error! Something went wrong!");
+			}
 
-	    // Uses generated seed to commence Trust operation
-    	System.out.println("\n~~~Using above Secret Seed to commence Trust Operation~~~");
-        KeyPair source;
-        
-        try {
-            	source = KeyPair.fromSecretSeed(pair.getSecretSeed());
-            	TimeUnit.SECONDS.sleep(1); //wait 1 second
-        }
-        catch (Exception e) {
-            	throw new RuntimeException("Error! Something went wrong!");
-        }
-        
-        // Asks user for amount of Asset (TBC) to Trust
-        try {
-        	System.out.println("\nEnter the amount of TBC(Currency) to Trust: ");
-        	TimeUnit.SECONDS.sleep(1); //wait 1 second
-    	}
-        catch (Exception e) {
-        	throw new RuntimeException("Error! Something went wrong!");
-    	}
-        	String amount = scanner.nextLine();
-		
+		// Uses generated seed to commence Trust operation
+		System.out.println("\n~~~Using above Secret Seed to commence Trust Operation~~~");
+		KeyPair source;
+
+		try {
+			source = KeyPair.fromSecretSeed(pair.getSecretSeed());
+			TimeUnit.SECONDS.sleep(1); //wait 1 second
+			}
+		catch (Exception e) {
+			throw new RuntimeException("Error! Something went wrong!");
+			}
+
+		// Asks user for amount of Asset (TBC) to Trust
+		try {
+			System.out.println("\nEnter the amount of TBC(Currency) to Trust: ");
+			TimeUnit.SECONDS.sleep(1); //wait 1 second
+			}
+		catch (Exception e) {
+			throw new RuntimeException("Error! Something went wrong!");
+			}
+		String amount = scanner.nextLine();
+
 		//1. Represent the Asset
 		Asset TBC = Asset.createNonNativeAsset("TBC", issuingKeys);
 
@@ -80,41 +78,42 @@ public class AutoAccountTrustLimit {
 		AccountResponse receiving = null;
 		try {
 			receiving = server.accounts().account(source);
-        	TimeUnit.SECONDS.sleep(1); //wait 1 second
-		}
-	    catch (Exception e) {
-	        	throw new RuntimeException("Error! Something went wrong!");
-	       	}		
+			TimeUnit.SECONDS.sleep(1); //wait 1 second
+			}
+		catch (Exception e) {
+			throw new RuntimeException("Error! Something went wrong!");
+			}
 		Transaction allowTBC = new Transaction.Builder(receiving)
-		  .addOperation(
+				.addOperation(
 		//2.1. ChangeTrust operation creates (or alters) a TrustLine
 		// Second parameter limits the amount
-		    new ChangeTrustOperation.Builder(TBC, amount).build())
-		  .build();
+						new ChangeTrustOperation.Builder(TBC, amount).build())
+				.build();
 		//2.2 sign using above source Secret Seed
 		allowTBC.sign(source);
-		
+
 		//3. Display Ledger number and Transaction Hash if it was a success
 		try {
 			SubmitTransactionResponse res = server.submitTransaction(allowTBC);
 			if (!res.isSuccess() == false) {
-			System.out.println("\nCONGRATULATIONS! You can now use "+TBC+" Cryptocurrency!");
-        	System.out.println("\nLedger Number:\n" + res.getLedger());
-        	System.out.println("Transaction Hash:\n" + res.getHash());
-        	TimeUnit.SECONDS.sleep(1); //wait 1 second
+				System.out.println("\nCONGRATULATIONS! You can now use [TBC: "+TBC+"] Cryptocurrency!");
+				System.out.println("\nLedger Number:\n" + res.getLedger());
+				System.out.println("Transaction Hash:\n" + res.getHash());
+				TimeUnit.SECONDS.sleep(1); //wait 1 second
+				}
+			} 
+		catch (Exception e) {
+			throw new RuntimeException("\nError! Something went wrong! Please try again.");
 			}
-		} 
-	    catch (Exception e) {
-        	throw new RuntimeException("\nError! Something went wrong! Please try again.");
+
+		// balance, AccountResponse: fetches account balances for generated account by getting current sequence
+		AccountResponse account = server.accounts().account(KeyPair.fromAccountId(pair.getAccountId()));
+		System.out.println("\nBalances for account: \n" + pair.getAccountId());
+		for (AccountResponse.Balance balance : account.getBalances()) {
+			System.out.println("\nType: " + balance.getAssetType());
+			System.out.println("Code: " + balance.getAssetCode());
+			System.out.println("Limit: " + balance.getLimit());
+			System.out.println("Balance: " + balance.getBalance());
+			}
 		}
-        // balance, AccountResponse: fetches account balances for generated account by getting current sequence
-        AccountResponse account = server.accounts().account(KeyPair.fromAccountId(pair.getAccountId()));
-        System.out.println("\nBalances for account: \n" + pair.getAccountId());
-        for (AccountResponse.Balance balance : account.getBalances()) {
-    		System.out.println("\nType: " + balance.getAssetType());
-    		System.out.println("Code: " + balance.getAssetCode());
-    		System.out.println("Limit: " + balance.getLimit());
-    		System.out.println("Balance: " + balance.getBalance());
-        }
-    }
-}
+	}
